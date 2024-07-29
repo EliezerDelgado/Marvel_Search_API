b09a8e214ffd57789fc391aa77bde4f408b74765
eliezer <delgadoeliezer05@gmail.com>
2024-07-29 10:35:13 +0200

    Init 8

--- app/src/main/java/com/eliezer/marvel_characters/models/responses/CharacterWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.eliezer.marvel_characters.models.responses
-
-data class CharacterWrapper(
-    val code: Int,
-    val status: String,
-    val copyright: String,
-    val data: DataContainer,
-)
