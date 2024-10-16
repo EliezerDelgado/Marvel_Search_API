8b36bfff0534eeb7611f774c8287a9ee5e79c543
eliezer <delgadoeliezer05@gmail.com>
2024-10-16 17:29:59 +0200

    Commit 36

--- app/src/main/java/com/eliezer/marvel_search_api/domain/local_property/LocalDatabase.kt
+++ app/src/main/java/com/eliezer/marvel_search_api/domain/local_property/LocalDatabase.kt
@@ -11,7 +11,7 @@ object LocalDatabase {
     {
         db = Room.databaseBuilder(
             context,
-            MarvelFavoriteDatabase::class.java, "marvelDB"
+            MarvelFavoriteDatabase::class.java, "favoriteMarvelDB"
         ).build()
     }
 }
